fd4850eb64e805e30c48e7a128d452b8f900f137
Volodymyr Balamut <volodymyrbalamut@gmail.com>
2018-04-09 23:03:57 +0300

    RESTful API for Clip

--- src/main/java/com/worldmusic/WorldMusicSpring/services/ClipService.java
+++ src/main/java/com/worldmusic/WorldMusicSpring/services/ClipService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ClipService {
@@ -20,9 +21,21 @@ public class ClipService {
                 .forEach(clips::add);
         return clips;
     }
+    public Optional<Clip> getClip(int id){
+            return clipRepository.findById(id);
+    }
+
 
     public Clip addClip(Clip clip){
         clipRepository.save(clip);
         return clip;
+    }
+    public Clip updateClip(Clip clip){
+        clipRepository.save(clip);
+        return clip;
+    }
+    public boolean deleteClip(int id){
+        clipRepository.deleteById(id);
+        return true;
     }
 }
