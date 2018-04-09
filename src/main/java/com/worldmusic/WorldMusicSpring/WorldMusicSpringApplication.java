e1973942c0bc6d978e18d0655575d3b062b0a782
Volodymyr Balamut <volodymyrbalamut@gmail.com>
2018-04-09 18:35:47 +0300

    "first commit"

---
+++ src/main/java/com/worldmusic/WorldMusicSpring/WorldMusicSpringApplication.java
@@ -0,0 +1,12 @@
+package com.worldmusic.WorldMusicSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorldMusicSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorldMusicSpringApplication.class, args);
+	}
+}
