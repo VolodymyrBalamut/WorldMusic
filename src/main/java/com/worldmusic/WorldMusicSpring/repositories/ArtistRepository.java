40540313890f21c85260178a86fcc59bbca93ffc
MinaevK <minaevk13@gmail.com>
2018-04-10 21:40:03 +0300

    Add service

---
+++ src/main/java/com/worldmusic/WorldMusicSpring/repositories/ArtistRepository.java
@@ -0,0 +1,8 @@
+package com.worldmusic.WorldMusicSpring.repositories;
+
+import com.worldmusic.WorldMusicSpring.model.Artist;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ArtistRepository extends CrudRepository<Artist,Integer>{
+
+}
---
+++ src/main/java/com/worldmusic/WorldMusicSpring/services/ArtistService.java
@@ -0,0 +1,40 @@
+package com.worldmusic.WorldMusicSpring.services;
+
+import com.worldmusic.WorldMusicSpring.model.Artist;
+import com.worldmusic.WorldMusicSpring.repositories.ArtistRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ArtistService {
+    @Autowired
+    private ArtistRepository artistRepository;
+
+    public List<Artist> getAllArtists(){
+        List<Artist> artists = new ArrayList<>();
+        artistRepository.findAll()
+                .forEach(artists::add);
+        return artists;
+    }
+    public Optional<Artist> getArtist(int id){
+        return artistRepository.findById(id);
+    }
+
+
+    public Artist addArtist(Artist artist){
+        artistRepository.save(artist);
+        return artist;
+    }
+    public Artist updateArtist(Artist artist){
+        artistRepository.save(artist);
+        return artist;
+    }
+    public boolean deleteArtist(int id){
+        artistRepository.deleteById(id);
+        return true;
+    }
+}
