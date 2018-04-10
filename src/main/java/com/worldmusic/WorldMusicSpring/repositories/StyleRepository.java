8122ca09c0c2e1091a5fb7ef6ce0ebfa67073ba4
AntonBoloban <boloban91@mail.ru>
2018-04-11 00:52:37 +0300

    Update StyleRepository.java

--- src/main/java/com/worldmusic/WorldMusicSpring/repositories/StyleRepository.java
+++ src/main/java/com/worldmusic/WorldMusicSpring/repositories/StyleRepository.java
@@ -2,7 +2,8 @@ package com.worldmusic.WorldMusicSpring.repositories;
 
 import com.worldmusic.WorldMusicSpring.model.Style;
 import org.springframework.data.repository.CrudRepository;
+import java.util.List;
 
-public interface StyleRepository extends CrudRepository<Style,Integer> {
-
+public interface StyleRepository extends CrudRepository<Style,Integer>{
+    List<Style> findByName(String name);
 }
