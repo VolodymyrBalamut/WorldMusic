1fb11a44e175b0329a1558b19a8e7f7518670d93
AntonBoloban <boloban91@mail.ru>
2018-04-25 14:43:42 +0300

    Delete CountryRepository.java

--- src/main/java/com/worldmusic/WorldMusicSpring/repositories/CountryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.worldmusic.WorldMusicSpring.repositories;
-
-import com.worldmusic.WorldMusicSpring.model.Country;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CountryRepository extends CrudRepository<Country,String> {
-
-}
