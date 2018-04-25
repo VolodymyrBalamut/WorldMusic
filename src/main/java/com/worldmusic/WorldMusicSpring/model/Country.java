d050ee859d9c5e90ab897aeac60e4d164126b1dc
AntonBoloban <boloban91@mail.ru>
2018-04-25 14:43:05 +0300

    Add files via upload

---
+++ src/main/java/com/worldmusic/WorldMusicSpring/model/Country.java
@@ -0,0 +1,31 @@
+package com.worldmusic.WorldMusicSpring.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="countries")
+public class Country {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String code_id;
+    private String name;
+
+    @OneToMany(mappedBy = "country")
+    private List<Clip> clips;
+
+
+    public Country(int id){
+        this.id = id;
+    }
+}
