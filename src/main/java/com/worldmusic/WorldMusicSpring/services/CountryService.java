a07e4a3bf6f7be833d0ec991453934ca109c6612
AntonBoloban <boloban91@mail.ru>
2018-04-25 14:44:34 +0300

    Add files via upload

---
+++ src/main/java/com/worldmusic/WorldMusicSpring/services/CountryService.java
@@ -0,0 +1,45 @@
+package com.worldmusic.WorldMusicSpring.services;
+
+import com.worldmusic.WorldMusicSpring.model.Country;
+import com.worldmusic.WorldMusicSpring.repositories.CountryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CountryService {
+
+    @Autowired
+    private CountryRepository countryRepository;
+
+    public List<Country> getAllCountries(){
+        List<Country> countries = new ArrayList<>();
+        countryRepository.findAll()
+                .forEach(countries::add);
+        return countries;
+    }
+    public Country getCountry(int id){
+        return countryRepository.findById(id).get();
+    }
+
+
+    public Country addCountry(Country country){
+        countryRepository.save(country);
+        return country;
+    }
+    public Country updateCountry(Country country){
+        countryRepository.save(country);
+        return country;
+    }
+    public boolean deleteCountry(int id){
+        countryRepository.deleteById(id);
+        return true;
+    }
+
+    public List<Country> getCountryByName(String name){
+        return countryRepository.findByName(name);
+    }
+}
